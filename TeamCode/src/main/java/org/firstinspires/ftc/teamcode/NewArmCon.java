82a2d407f4518688203d9e6208c7707339964fd2
Jeffrey <boby8k@gmail.com>
2024-01-22 16:30:17 -0500

    Big changes to all

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewArmCon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewArmCon.java
@@ -1,2 +1,46 @@
-package org.firstinspires.ftc.teamcode;public class NewArmCon {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+//Driver-Controlled Period
+@TeleOp
+public class NewArmCon extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        int count = 0;
+        GeneralDrive myRobot = new GeneralDrive(hardwareMap);
+        Telemetry telemetry;
+        telemetry = this.telemetry;
+
+        waitForStart();
+
+        while ( opModeIsActive() ) {
+
+            //Down
+            if (gamepad1.right_trigger>0){
+                myRobot.setLeftDrivePower(gamepad1.right_trigger);
+                myRobot.setRightDrivePower(-gamepad1.right_trigger);
+                telemetry.addData("Status", "Going down...");
+                telemetry.update();
+
+            }
+
+            //Out
+            if (gamepad1.left_trigger>0) {
+                myRobot.setLeftDrivePower(-gamepad1.right_trigger);
+                myRobot.setRightDrivePower(gamepad1.right_trigger);
+                telemetry.addData("Status", "Going out...");
+                telemetry.update();
+            }
+        }
+    }
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewGamepadDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewGamepadDrive.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.control;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -8,35 +8,48 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 //Driver-Controlled Period
 @TeleOp
 public class NewGamepadDrive extends LinearOpMode {
 
-
     @Override
     public void runOpMode() {
-        boolean turning90 = false;
+        int count = 0;
         GeneralDrive myRobot = new GeneralDrive(hardwareMap);
+        Telemetry telemetry;
+        telemetry = this.telemetry;
+
         waitForStart();
 
         while ( opModeIsActive() ) {
             double drive = gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
 
-            if(gamepad1.left_bumper && turning90 == false){
-                turning90 = true;
+            if(gamepad1.left_bumper){
                 myRobot.turnLeft90();
-                turning90 = false;
+                telemetry.addData("Status", "Turning left " + count);
+                telemetry.update();
+
             }
-            else if(gamepad1.right_bumper && turning90 == false){
-                turning90 = true;
+            else if(gamepad1.right_bumper){
                 myRobot.turnRight90();
-                turning90 = false;
+                telemetry.addData("Status", "Turning left " + count);
+                telemetry.update();
+
             }
-            else{
+            else if (gamepad1.right_trigger>0){
+                myRobot.moveForward(gamepad1.right_trigger);
+            }
+            else if (gamepad1.left_trigger>0){
+                myRobot.moveBackward(gamepad1.left_trigger);
+            }
+            else {
                 myRobot.setLeftDrivePower(drive - turn);
                 myRobot.setRightDrivePower(drive + turn);
             }
+
 
         }
     }
